f59f3b677b22ddbddbf94dd27d6d99f212c5ce92
lucastheldl <lucastheldl@gmail.com>
2022-11-22 18:34:09 -0300

    Scrollbar style and js fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//Carousel slider
 const carousel = document.querySelector(".carousel");
 const firstCard = document.querySelectorAll(".card")[0];
 const arrow = document.querySelectorAll(".arrowbtn");
@@ -8,3 +9,35 @@ arrow.forEach((icon) => {
     carousel.scrollLeft += icon.id == "left" ? -firstCardWidth : firstCardWidth;
   });
 });
+
+//Parallax effect
+let stars = window.document.querySelector("img#stars");
+let back = window.document.querySelector("img#background");
+let mBack = window.document.querySelector("img#character");
+//let text = window.document.querySelector("h2#text")
+//let btn = window.document.querySelector("a#btn")
+let mFront = window.document.querySelector("img#foreground");
+// let header = window.document.querySelector("header")
+
+window.addEventListener("scroll", scrolling);
+
+function scrolling() {
+  let value = window.scrollY;
+  stars.style.left = value * 0.25 + "px";
+  back.style.top = value * 0.8 + "px";
+  mBack.style.top = value * 0.5 + "px";
+  mFront.style.top = value * 0 + "px";
+}
+
+//Change stories display
+function changeDisplay(id) {
+  let itemList = window.document.getElementsByClassName("content");
+
+  for (let i = 0; i < itemList.length; i++) {
+    if (itemList[i].id != id) {
+      itemList[i].style.display = "none";
+    } else {
+      itemList[i].style.display = "block";
+    }
+  }
+}
